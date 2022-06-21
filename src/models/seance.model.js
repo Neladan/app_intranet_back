4ffbfef2e9f9011b06c6ff95961b734430da724a
Lionel ADANKON <lionelad006@gmail.com>
2022-06-21 11:23:25 +0100

    Seance finished

--- src/models/seance.model.js
+++ src/models/seance.model.js
@@ -1,6 +1,6 @@
 var dbConn = require('../../config/db.config');
 
-var Seance = (seance) => {
+var Seance = function(seance) {
     this.idSeance = seance.idSeance;
     this.date = new Date()
     this.heureDebut = new TimeRanges();
@@ -24,4 +24,59 @@ Seance.getSeanceList = (result) => {
     })
 }
 
+//get seance by ID
+Seance.getSeanceByID = (idSEance, result) => {
+    dbConn.query('SELECT * FROM Seance WHERE idSeance=?', idSeance, (err, res)=> {
+        if(err){
+            console.log("Error while fetching seance by ID", idSeance);
+            result(null, err);
+        }
+        else {
+            result(null, res); 
+        }
+    })
+}
+
+// create new seance
+Seance.createSeance = (seanceReqData, result) =>{
+    dbConn.query('INSERT INTO Seance SET ? ', seanceReqData,(err, res) => {
+        if(err){
+            console.log('Error wile inserting data');
+            result(null, err);
+        }
+        else {
+            console.log('Seance created successfully');
+            result(null, res);
+        }
+    })
+}
+
+// update seance
+Seance.updateSeance = (idSeance, seanceReqData, result) => {
+    dbConn.query("UPDATE Seance SET date=?,heureDebut=?,heureFin=?,numModule=?,codeTypePermis=?,codeExamen=? WHERE idSeance=?", [seanceReqData.date, seanceReqData.heureDebut, seanceReqData.heureFin, seanceReqData.numModule, seanceReqData.codeTypePermis, seanceReqData.codeExamen, idSeance], (err, res) => {
+        if(err){
+            console.log('Error wile updating data');
+            result(null, err);
+        }
+        else {
+            console.log('Seance updated successfully');
+            result(null, res);
+        }
+    })
+}
+
+// delete seance
+Seance.deleteSeance = (idSeance, result) => {
+    dbConn.query    ("DELETE FROM Seance WHERE idSeance=?", [idSeance], (err, res) => {
+        if(err){
+            console.log('Error wile deleting data');
+            result(null, err);
+        }
+        else {
+            console.log('Seance deleted successfully');
+            result(null, res);
+        }
+    })
+}
+ 
 module.exports = Seance;
